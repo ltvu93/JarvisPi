a68ca64c0154f73033f7211941efcdbe75b7b7cb
ltvu <ltvu872003@gmail.com>
2016-01-19 22:55:27 +0700

    them evernote

---
+++ functions/evernote.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from core import tts
+
+from evernote.api.client import EvernoteClient
+
+def handle(mic, comamnd, profile):
+	"""
+	Go to link: https://dev.evernote.com/doc/start/python.php
+	for guide
+	"""
+
+	auth_token = profile['evernote']['token']
+    client = EvernoteClient(token=auth_token)
+    note_store = client.get_note_store()
+    
+    note = Types.Note()
+    note.title = "Ghi chú trên JarvisPi"
+	tts.espeak_tts("Nội dung ghi chú là gì?")
+    content = mic.activeListen()
+    note.content = '<?xml version="1.0" encoding="UTF-8"?>'
+    note.content += '<!DOCTYPE en-note SYSTEM ' \
+    '"http://xml.evernote.com/pub/enml2.dtd">'
+    note.content += '<en-note>Note:<br/>'
+    note.content += ('%s' % content)
+    note.content += '</en-note>'
+
+    created_note = note_store.createNote(note)
+    tts.espeak_tts("Ghi chú thành công")
+
+    #TODO: Add funtions read notes
+
+def isMatch(command):
+    return command == u"GHI CHÚ"
